fb343a93eea591979313e3f989c274639f213ba3
gizmore <gizmore@wechall.net>
2020-10-28 15:34:54 +0100

    6.10-r9326

--- Module_JQueryAutocomplete.php
+++ Module_JQueryAutocomplete.php
@@ -3,9 +3,11 @@ namespace GDO\JQueryAutocomplete;
 
 use GDO\Core\GDO_Module;
 use GDO\Core\Module_Core;
+use GDO\DB\GDT_Checkbox;
 
 /**
- * EasyAutocomplete wrapper module
+ * EasyAutocomplete wrapper module.
+ * 
  * @author gizmore
  * @version 6.10
  * @since 6.10
@@ -16,13 +18,24 @@ final class Module_JQueryAutocomplete extends GDO_Module
     
     public function getDependencies() { return ['JQuery']; }
     
+//     public function getConfig()
+//     {
+//         return array(
+//             GDT_Checkbox::make('use_easy_autocomplete')->initial('1'),
+//         );
+//     }
+//     public function cfgEasyComplete() { return $this->getConfigValue('use_easy_autocomplete'); }
+    
     public function onIncludeScripts()
     {
         $min = Module_Core::instance()->cfgMinifyJS() === 'no' ? '' : '.min';
-        $this->addCSS("EasyAutocomplete/dist/easy-autocomplete{$min}.css");
-        $this->addCSS("EasyAutocomplete/dist/easy-autocomplete.themes{$min}.css");
-        $this->addJavascript("EasyAutocomplete/dist/jquery.easy-autocomplete{$min}.js");
-        $this->addJavascript("js/gdo6-easy-autocomplete.js");
+//         if ($this->cfgEasyComplete())
+//         {
+            $this->addCSS("EasyAutocomplete/dist/easy-autocomplete{$min}.css");
+            $this->addCSS("EasyAutocomplete/dist/easy-autocomplete.themes{$min}.css");
+            $this->addJavascript("EasyAutocomplete/dist/jquery.easy-autocomplete{$min}.js");
+            $this->addJavascript("js/gdo6-easy-autocomplete.js");
+//         }
     }
     
 }
--- js/gdo6-easy-autocomplete.js
+++ js/gdo6-easy-autocomplete.js
@@ -3,6 +3,24 @@ $(function(){
 	$('.gdo-autocomplete').each(function(){
 		var $this = $(this);
 		var config  = JSON.parse($this.attr('data-config'));
+		console.log(config);
+		var $hidden = $('#completion-'+config.name);
+		var name = $this.attr('name');
+		$this.attr('name', '');
+		$hidden.attr('name', name);
+		$this.attr('placeholder', config.emptyLabel);
+		$hidden.val(config.selected.id);
+		if (config.selected) {
+			if (config.multiple) {
+				console.error('MULTUPLE COMPLETION NOT SUPPORTED YET.');
+			} else {
+				if (config.selected.id !== config.emptyValue) {
+					$this.val(config.selected.text);
+				} else {
+					$this.val('');
+				}
+			}
+		}
 		var options = {
 			url: function (query) {
 				return config.completionHref + '&fmt=json&query=' + query;
@@ -14,23 +32,16 @@ $(function(){
 		        	return item.display;
 		        }
 		    },
+//		    theme: 'round',
 		    list: {
-		    	onChooseEvent: function() {
+				maxNumberOfElements: 20,
+				onChooseEvent: function() {
 		            var selectedItemValue = $this.getSelectedItemData().id;
-		            $('#completion-'+config.name).val(selectedItemValue);
+		            $hidden.val(selectedItemValue);
 		        },
 		    }
 		};
 		$this.easyAutocomplete(options);
 	});
 
-	$('.gdo-autocomplete-enum').each(function(){
-		var $this = $(this);
-		var config  = JSON.parse($this.attr('data-config'));
-		console.log(config);
-		var options = {
-			
-		};
-		
-	});
 });
